1a94557889c77c7086b91dc8e3943639da032995
Sebs <sebastianruizj2014@gmail.com>
2023-09-14 07:41:47 -0500

    SASS y Gulp

--- Cafeteria/gulpfile.js
+++ Cafeteria/gulpfile.js
@@ -10,10 +10,10 @@ function css(done) {
      * 2 - compilarla
      * 3 - guardar el .css
      */
-    src('src/scss/app.scss')
-        .pipe(sass())
+    src('src/scss/app.scss') // indetificar archivo
+        .pipe(sass()) // compilarla
         .pipe(postcss([autoprefixer()]))
-        .pipe(dest('build/css'));
+        .pipe(dest('build/css')); // guardar el .css
 
     done();
 }
@@ -22,13 +22,9 @@ function dev() {
     watch('src/scss/app.scss', css);
 }
 
-function tareaDefault() {
-    console.log('Soy la tarea default');
-}
-
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css, dev);
 
 // series - Se inicia una tarea, y hasta que finaliza, se inicia la siguiente.
 // parallel - Todas inician al mismo tiempo
+exports.default = series(css, dev);
