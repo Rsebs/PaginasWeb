fc0719be3faf365177a5541549607fc35c46d22f
Sebs <sebastianruizj2014@gmail.com>
2023-09-14 16:58:05 -0500

    SASS, Gulp, Webp, Avif
    Nuevas dependencias añadidas

--- Cafeteria/gulpfile.js
+++ Cafeteria/gulpfile.js
@@ -1,7 +1,14 @@
 const {src, dest, watch, series, parallel} = require('gulp');
+
+// CSS y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+// Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     // Compilar SASS
@@ -18,13 +25,44 @@ function css(done) {
     done();
 }
 
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe(imagemin({optimizationLevel: 3}))
+        .pipe(dest('build/img'));
+}
+
+function versionWebp() {
+    const opciones = {
+        quality: 50
+    }
+
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+}
+
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    }
+
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'));
+}
+
 function dev() {
-    watch('src/scss/app.scss', css);
+    // watch('src/scss/header/_header.scss', css);
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
 }
 
 exports.css = css;
 exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
 
 // series - Se inicia una tarea, y hasta que finaliza, se inicia la siguiente.
 // parallel - Todas inician al mismo tiempo
-exports.default = series(css, dev);
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
